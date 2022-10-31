687cccdd5c2794401aab4346805933f890aeb942
Erin Kado-Fong <erin.kado-fong@yale.edu>
2022-10-31 16:40:39 -0400

    start QA mapping

---
+++ runs/qa/map_dataproducts.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def get_all_tracts ( repo ):
+    tracts = []
+    for data_id in butler.registry.queryDataIds(
+            'tract',
+            datasets="visitSummary",
+            collections="DECam/runs/merian/w_2022_29",
+            instrument="DECam",
+            skymap='hsc_rings_v1'
+        ):    
+        tract_id = data_id['tract']
+        tracts.append(tract_id)
+    return set(tracts)
